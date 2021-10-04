ab50f746012013d735dc94231e66005eb53ed16a
Levi Hechenberger Di Valentino <68319554+levvij@users.noreply.github.com>
2021-10-04 20:37:23 +0200

    add esprima tokenization + eval + Function ban

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,14 +1,41 @@
+import * as esprima from "esprima";
+
 export class Sandbox {
     exposedVariables;
 
     constructor(private source: string) {
+        const tokens = esprima.tokenize(source);
+
+        for (let token of tokens) {
+            switch (token.value) {
+                case "function": {
+                    throw new Error("Unsafe sandbox input. Convert function() to arrow functions.");
+
+                    break;
+                }
+
+                case "eval":
+                case "Function": {
+                    throw new Error("Unsafe sandbox input. The 'eval' and 'Function' keywords cannot be used in a sandbox.");
+
+                    break;
+                }
+            }
+        }
+
         // check for function() in source
         // you could use the following code to escape the sandbox because referencing this in a function will return the globalThis
         //
         //    const globals = (function () { return this })();
         //
         if (/function\s*([_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*)?\s*\(/.test(source)) {
-            throw new Error("Unsafe sandbox input. Convert function() to arrow functions.");
+            
+        }
+
+        // eval is a very special case
+        // even tho eval is set to null by the closure and reading 'eval' returns null, calling eval still works
+        if (/Function/.test(source)) {
+            
         }
 
         this.reset();
